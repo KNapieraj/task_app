d90f5da089cb6787757558f7207e00150e470852
Kamil Napieraj <knapieraj616@gmail.com>
2023-05-18 19:25:55 +0200

    task app works

--- zadanie/zadanie/urls.py
+++ zadanie/zadanie/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for zadanie project.
+"""taskapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('zadanie_app/', include('zadanie_app.urls'))
+    path('task/', include('zadanie_app.urls'), name="task-dashboard"),
+    path('', RedirectView.as_view(url='/task/', permanent=False)),
 ]
